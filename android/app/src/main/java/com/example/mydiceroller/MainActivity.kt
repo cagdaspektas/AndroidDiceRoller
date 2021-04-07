0ef0ba75b36690bbf80aa2021d60881236e6b881
cagdaspektas <cagdaspektas3405@gmail.com>
2021-04-07 15:40:17 +0300

    Dice Roller

---
+++ android/app/src/main/java/com/example/mydiceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mydiceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var rollButton:Button = findViewById(R.id.roll_button)
+        //rollButton.text="Salla bakalım" yaparak dinamik şekilde kullanılabilinir.
+        rollButton.setOnClickListener {
+          ZarAt()
+        }
+    }
+
+    private fun ZarAt() {
+
+    var randomInt=  Random.nextInt(6)+1
+      var drawableResource=  when(randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
+    var diceImage:ImageView=findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+
+
+    }
+}
